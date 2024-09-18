80e7882ff27f8c018a439ab23cffb317502c0a89
Onlyuser <jrlumpkinjr@gmail.com>
2024-09-18 19:23:27 -0400

    tracks added to playlists

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 // require all essential modules at the top of each controller
 const Track = require('../models/track.js');
+const User = require('../models/user.js');
 
 router.get('/new', async (req, res) => {
     res.render('tracks/new.ejs');
@@ -18,7 +19,25 @@ router.post("/", async (req, res) => {
     }
   });
 
-  
+  // routes below are for adding tracks into playlists
+router.put('/add/:playlistId', async (req, res) => {
+    const user = await User.findById(req.session.user._id);
+    const playlist = user.playlists.id(req.params.playlistId);
+    const track = await Track.findById(req.body.trackId)
+    try {
+        playlist.tracks.push(track._id);
+        await user.save();
+        track.playlists.push(playlist._id);
+        await track.save();
+        res.redirect(`/users/${user._id}/playlists/${playlist._id}`);
+    } catch (error) {
+        console.log(error)
+        res.redirect(`/users/${user._id}/playlists/${playlist._id}`);
+    }
+});
+
+
+
 module.exports = router;
 // exports your routers for use in server.js
 
